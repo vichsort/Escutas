a400164da7f0be5b4eee081ac37268f99b269ebc
wolnei mignoni <vichsort@proton.me>
2026-02-01 19:27:32 -0300

    feat: rewrite token every 7 days (expiration on API)

--- src/services/api_service.js
+++ src/services/api_service.js
@@ -17,4 +17,20 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            console.warn('Sessão expirada. Deslogando usuário...')
+            localStorage.removeItem('escutas_token')
+            localStorage.removeItem('escutas_user')
+
+            if (window.location.pathname !== '/') {
+                window.location.href = '/'
+            }
+        }
+        return Promise.reject(error)
+    }
+)
+
 export default api;
