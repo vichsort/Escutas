984cc15f9ecec63a50d498eda016ef54e38c0389
Vitor Mignoni <vichsort@proton.me>
2026-05-11 16:24:08 -0300

    feat: new layout for review page, sidebar excluded - need rework on review injection

---
+++ src/layouts/ReviewLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="h-screen w-full overflow-hidden bg-background-light dark:bg-black text-gray-900 dark:text-white transition-colors duration-300">
+    <main class="h-full overflow-y-auto relative bg-background-light dark:bg-background-dark">
+      <div class="min-h-full">
+        <header class="sticky top-0 z-10 p-4 flex justify-end">
+          <slot name="header-actions" />
+        </header>
+
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
