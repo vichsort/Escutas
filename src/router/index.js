68db952853eafab52968419f52f29b5d3f2e9e58
Vitor Mignoni <vichsort@proton.me>
2026-05-17 12:31:37 -0300

    refactor: exposed routes for every blog endpoint

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,31 @@ import { useLoadingStore } from '@/stores/loading_store' // <-- Import da store 
 
 const HomeView = () => import('@/views/HomeView.vue')
 const SearchView = () => import('@/views/SearchView.vue')
-const BlogList = () => import('@/views/blog/BlogList.vue')
 const Callback = () => import('@/views/auth/Callback.vue')
 const LibraryView = () => import('@/views/LibraryView.vue')
 const DraftsView = () => import('@/views/DraftsView.vue')
 const ReviewDetailsView = () => import('@/views/reviews/ReviewDetailsView.vue')
 const CreateReviewView = () => import('@/views/CreateReviewView.vue')
 const ArtistView = () => import('@/views/ArtistView.vue')
+const BlogHomeView = () => import('@/views/blog/Index.vue')
+const BlogWriteView = () => import('@/views/blog/WriteView.vue')
+const BlogPostView = () => import('@/views/blog/PostView.vue')
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'home', component: HomeView },
         { path: '/search', name: 'search', component: SearchView },
-        { path: '/blog', name: 'blog', component: BlogList },
         { path: '/auth/callback', name: 'auth-callback', component: Callback },
         { path: '/library', name: 'library', component: LibraryView, meta: { requiresAuth: true } },
         { path: '/library/drafts', name: 'drafts', component: DraftsView, meta: { requiresAuth: true } },
         { path: '/reviews/:id', name: 'review-details', component: ReviewDetailsView, meta: { requiresAuth: true } },
         { path: '/albums/:id/review', name: 'create-review', component: CreateReviewView, meta: { requiresAuth: true, layout: 'review' } },
         { path: '/artists/:id', name: 'artist', component: ArtistView, meta: { requiresAuth: true } },
+        { path: '/blog', name: 'blog-home', component: BlogHomeView },
+        { path: '/blog/write', name: 'blog-write', component: BlogWriteView, meta: { requiresAuth: true } },
+        { path: '/blog/write/:id', name: 'blog-edit', component: BlogWriteView, meta: { requiresAuth: true } },
+        { path: '/blog/:slug', name: 'blog-post', component: BlogPostView, meta: { requiresAuth: true } },
     ]
 })
 
