67a129cee51bfdc88bd15a2aeba4a8ff78303cdc
Vitor Mignoni <vichsort@proton.me>
2026-05-17 12:32:35 -0300

    chore: remove unused deprecated blog file

--- src/views/blog/BlogList.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-8">
-    <h1 class="text-3xl font-bold mb-4">Blog List</h1>
-    <p class="text-gray-600 dark:text-gray-400">Página em construção...</p>
-  </div>
-</template>
